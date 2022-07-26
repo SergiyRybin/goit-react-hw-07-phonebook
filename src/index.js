bd6de26555512c092a3f4537755947585196cfc8
SergiyRybin <serhiobax@gmail.com>
2022-07-26 12:15:04 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import App from 'components/App';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import './index.css';
-import { store } from './redux/store';
+import App from "components/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { store } from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+      <Provider store={store}>
         <App />
-    </Provider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
