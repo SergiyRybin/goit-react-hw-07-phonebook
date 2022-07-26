927239280a7974504cf271f14cd2d4dfd6aa019b
SergiyRybin <serhiobax@gmail.com>
2022-07-26 20:40:46 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,8 @@
-import { Route, Routes } from "react-router";
 import MainFrom from "./MainForm/MainForm";
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="register" />
-        <Route path="login" />
-        <Route path="contacts" />
-      </Routes>
       <MainFrom />
     </>
   );
