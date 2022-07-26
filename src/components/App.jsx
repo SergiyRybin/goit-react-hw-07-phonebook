56094edeadf8039e8415f346bc7ffb02b0cc959a
SergiyRybin <serhiobax@gmail.com>
2022-07-26 12:30:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-import MainFrom from './MainForm/MainForm';
+import { Route, Routes } from "react-router";
+import MainFrom from "./MainForm/MainForm";
 
- const App = () => {
+const App = () => {
   return (
     <>
+      <Routes>
+        <Route path="register" />
+        <Route path="login" />
+        <Route path="contacts" />
+      </Routes>
       <MainFrom />
     </>
   );
 };
-export default App
+export default App;
